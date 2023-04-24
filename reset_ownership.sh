c434ae953b45474d1031dbd83bf8700b81455ec9
Krzysztof Sobieraj <Krzysztof.Sobieraj@StepStone.com>
2023-04-24 22:43:21 +0200

    add scripts to reset and restore permissions

---
+++ reset_ownership.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change ownership to pi:pi
+chown -R pi:pi grafana-config
+chown -R pi:pi grafana-data
+chown -R pi:pi loki-config
+chown -R pi:pi loki-data
+chown -R pi:pi prometheus-data
+
+echo "Ownership reset to pi:pi for all directories."
---
+++ restore_ownership.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Restore ownership
+chown -R 472:472 grafana-config
+chown -R 472:472 grafana-data
+chown -R 10001:10001 loki-config
+chown -R 10001:10001 loki-data
+chown -R 65534:65534 prometheus-data
+
+echo "Ownership restored for all directories."
