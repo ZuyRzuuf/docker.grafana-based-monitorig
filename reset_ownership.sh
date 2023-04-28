c34a438a7e7ad440027c0532922540432ea28d5c
Krzysiek <krzysiek@sobieraj.mobi>
2023-04-28 18:35:07 +0200

    fix elasticsearch container issues

--- reset_ownership.sh
+++ reset_ownership.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Change ownership to pi:pi
+# Change ownership to ${USER}
 chown -R ${USER}:${USER} grafana-config
 chown -R ${USER}:${USER} grafana-data
 chown -R ${USER}:${USER} grafana-provisioning
+chown -R ${USER}:${USER} grafana-plugins-bundled
 chown -R ${USER}:${USER} prometheus-data
+chown -R ${USER}:${USER} elasticsearch-data
 
 echo "Ownership reset to ${USER}:${USER} for all directories."
--- restore_ownership.sh
+++ restore_ownership.sh
@@ -4,6 +4,7 @@
 chown -R 472:472 grafana-config
 chown -R 472:472 grafana-data
 chown -R 472:472 grafana-provisioning
+chown -R 472:472 grafana-plugins-bundled
 chown -R 65534:65534 prometheus-data
 
 echo "Ownership restored for all directories."
